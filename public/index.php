05efb7bbfe43b911bc0b678c2cd3aee572eea93a
Isaac Colls <isaaccolls@MacBook-Air-de-Isaac.local>
2017-09-07 02:35:06 -0300

    Renderizando las vistas desde un método

--- public/index.php
+++ public/index.php
@@ -4,10 +4,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
-
 include_once '../config.php';
 
+$baseUrl = '';
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '',$_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://'.$_SERVER['HTTP_HOST'].$baseDir;
+define('BASE_URL', $baseUrl);
+
 $route = $_GET['route'] ?? '/';
+
+function render($fileName, $params=[]){
+	ob_start(); //todo lo q se imprima en vez de ir al navegador, va a un buffer
+	extract($params); //convierte nombres de un arreglo en variables
+	include $fileName;
+	return ob_get_clean(); //trae los datos q tiene el buffer y lo limpia
+}
 
 use Phroute\Phroute\RouteCollector;
 
@@ -17,7 +28,7 @@ $router->get('/', function() use ($pdo){
 	$query->execute();
 
 	$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-	include '../views/index.php';
+	return render('../views/index.php', ['blogPosts'=>$blogPosts]);
 });
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
