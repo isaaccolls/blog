c0a932c3815073b121411c2182cace32de8fc5e8
Isaac Colls <isaaccolls@MacBook-Air-de-Isaac.local>
2017-09-07 02:18:29 -0300

    Introducción a Router. Utilizando composer, con https://packagist.org/packages/phroute/phroute

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit04508773b84da6e1880923dfd09eb020
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit04508773b84da6e1880923dfd09eb020::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit04508773b84da6e1880923dfd09eb020::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
