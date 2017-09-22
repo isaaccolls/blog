eb81e54a1b9b2290454bc7901ec62604e2c197e5
Isaac Colls <isaaccolls@MacBook-Air-de-Isaac.local>
2017-09-22 00:33:32 -0300

    agregado illuminate/databes y elloquent para trabajar el Modelo

--- public/index.php
+++ public/index.php
@@ -4,21 +4,28 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
-include_once '../config.php';
 
 $baseUrl = '';
 $baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '',$_SERVER['SCRIPT_NAME']);
 $baseUrl = 'http://'.$_SERVER['HTTP_HOST'].$baseDir;
 define('BASE_URL', $baseUrl);
 
-$route = $_GET['route'] ?? '/';
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'cursophp',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
-function render($fileName, $params=[]){
-	ob_start(); //todo lo q se imprima en vez de ir al navegador, va a un buffer
-	extract($params); //convierte nombres de un arreglo en variables
-	include $fileName;
-	return ob_get_clean(); //trae los datos q tiene el buffer y lo limpia
-}
+$route = $_GET['route'] ?? '/';
 
 use Phroute\Phroute\RouteCollector;
 
